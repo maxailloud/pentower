76b5d7cf478b7e5b6b5535985286d60cabe971b1
Kryptos-FR <musset.nicolas@gmail.com>
2013-11-24 14:14:01 +0100

    TRANSLATED MouseOrbit script.

---
+++ Unity/Assets/Standard Assets/Scripts/Camera Scripts/MouseOrbit.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+using System.Collections;
+
+[AddComponentMenu("Camera-Control/Mouse Orbit")]
+public class MouseOrbit : MonoBehaviour
+{
+	public Transform target;
+	public float distance = 10.0f;
+	
+	public float xSpeed = 250.0f;
+	public float ySpeed = 120.0f;
+	
+	public float yMinLimit = -20f;
+	public float yMaxLimit = 80f;
+	
+	private float x = 0.0f;
+	private float y = 0.0f;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		var angles = transform.eulerAngles;
+		x = angles.y;
+		y = angles.x;
+		
+		// Make the rigid body not change rotation
+		if (rigidbody)
+			rigidbody.freezeRotation = true;
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate ()
+	{
+		if (target != null) {
+			x += Input.GetAxis("Mouse X") * xSpeed * 0.02f;
+			y -= Input.GetAxis("Mouse Y") * ySpeed * 0.02f;
+			
+			y = ClampAngle(y, yMinLimit, yMaxLimit);
+			
+			var rotation = Quaternion.Euler(y, x, 0f);
+			var position = rotation * new Vector3(0.0f, 0.0f, -distance) + target.position;
+			
+			transform.rotation = rotation;
+			transform.position = position;
+		}		
+	}
+	
+	static float ClampAngle (float angle, float min, float max) {
+		if (angle < -360f)
+			angle += 360f;
+		if (angle > 360f)
+			angle -= 360f;
+		return Mathf.Clamp (angle, min, max);
+	}
+}
